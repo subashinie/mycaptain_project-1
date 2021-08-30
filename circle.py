18192a81e3aa7dc562f6e5984ae558bae9b380c8
subashinie <87689475+subashinie@users.noreply.github.com>
2021-08-30 22:31:09 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 22:29:05 2021
+
+@author: suba
+"""
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
